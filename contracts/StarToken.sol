e007fda7ad88bd0cf5c162e40983751f034b3f16
Marcos Kichel <marcos.kichel@gmail.com>
2021-09-08 12:54:45 -0300

    Implemented StartToken initialization

--- contracts/StarToken.sol
+++ contracts/StarToken.sol
@@ -1,5 +1,13 @@
-pragma solidity ^0.5.4;
+pragma solidity ^0.7.3;
 
-contract StarToken {
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+
+contract StarToken is ERC777 {
+
+  constructor(
+    uint256 initialSupply
+  ) ERC777("StarToken", "STAR", new address[]) public {
+    _mint(msg.sender, msg.sender, initialSupply, "", "");
+  }
 
 }
