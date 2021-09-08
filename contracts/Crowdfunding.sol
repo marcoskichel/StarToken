e6b3b8049573a605d98c0648b541e396e5725d14
Marcos Kichel <marcos.kichel@gmail.com>
2021-09-08 13:10:38 -0300

    Added linter & formatter

--- contracts/Crowdfunding.sol
+++
@@ -1,4 +0,0 @@
-pragma solidity ^0.7.3;
-
-contract Crowdfunding {
-}
--- contracts/StarToken.sol
+++ contracts/StarToken.sol
@@ -1,12 +1,16 @@
-pragma solidity ^0.7.3;
+pragma solidity ^0.5.8;
 
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 
+/**
+* Implementation of the Star Token, a ERC777 token which is generated during crowdfundings
+**/
 contract StarToken is ERC777 {
 
   constructor(
     uint256 initialSupply
-  ) ERC777("StarToken", "STAR", new address[]) public {
+  ) public ERC777("StarToken", "STAR", new address[]) {
+    // Mint the initialSupply and add it to the deployer wallet
     _mint(msg.sender, msg.sender, initialSupply, "", "");
   }
 
